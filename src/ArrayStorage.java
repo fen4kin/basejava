93f5525488e3505e7ddef40a41d1cae54aebc8e8
fen4kin <fen4kin@gmail.com>
2019-03-19 20:32:09 +0500

    Update ArrayStorage.java
    lesson1 version2 without map

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,53 +3,60 @@ import java.util.*;
 /**
  * Array based storage for Resumes
  * <p>
- * Все действия в методах основаны на мапе, так как с ее помощью удобно получать доступ
- * как к экземпляру класса Resume, так и к его переменной. Более очевидных способов я не нашла.
+ * Второй вариант решения, без карты (если вдруг при проверке кто наткнется на первое)
  * <p>
- * При каждом взаимодействии с размером массива его приходится обнулять, иначе происходили бы лишние записи,
- * вроде uuid1, uuid1, uuid2, uuid1, uuid2, uuid3 при методе save(). Да и метод delete() по-другому просто не реализовать.
- * <p>
- * Как я уже писала, в моем решении все операции происходят в мапе.
- * Я не знала насколько необходимо каждый раз делать изменения именно в массиве storage.
- * Хоть и в моем варианте решения в массиве просто нет необходимости, я предполагаю, что есть какие-то "подводные камни",
- * вроде занимаемой памяти, быстродействия работы и совместимости с чем-то еще,
- * поэтому изменения storage все же затрагивали.
+ * Все действия реализуются непосредственно с массивом storage.
  */
 public class ArrayStorage {
-    private HashMap<String, Resume> mapOfResume = new HashMap<String, Resume>();
-    private Resume[] storage = new Resume[10000];
+    Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
-        mapOfResume.clear();
-        Arrays.fill(storage, null);
+        for (Resume resume : storage) {
+            resume = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
-        mapOfResume.put(r.uuid, r);
-        Arrays.fill(storage, null);
-        mapOfResume.values().toArray(storage);
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        return mapOfResume.get(uuid);
+        Resume getResume = null;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                getResume = storage[i];
+                break;
+            }
+        }
+        return getResume;
     }
 
     void delete(String uuid) {
-        Arrays.fill(storage, null);
-        mapOfResume.remove(uuid);
-        mapOfResume.values().toArray(storage);
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] array = new Resume[mapOfResume.size()];
-        mapOfResume.values().toArray(array);
+        Resume[] array = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = storage[i];
+        }
         return array;
     }
 
     int size() {
-        return mapOfResume.size();
+        return size;
     }
 }
