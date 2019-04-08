3cdf898ee0a6ee3c4148affe33ffdc934256fb42
fen4kin <fen4kin@gmail.com>
2019-04-09 01:37:20 +0500

    Resume: add equals and hashCode; ArrayStorage: Arrays: getAll and clear; Add methods: update and isPresent Change methods: delete, get, save

--- src/com/fen4kin/webapp/model/Resume.java
+++ src/com/fen4kin/webapp/model/Resume.java
@@ -1,5 +1,7 @@
 package com.fen4kin.webapp.model;
 
+import java.util.Objects;
+
 /**
  * Initial resume class
  */
@@ -19,5 +21,18 @@ public class Resume {
 
     public void setUuid(String uuid) {
         this.uuid = uuid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Resume resume = (Resume) o;
+        return Objects.equals(getUuid(), resume.getUuid());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUuid());
     }
 }
--- src/com/fen4kin/webapp/storage/ArrayStorage.java
+++ src/com/fen4kin/webapp/storage/ArrayStorage.java
@@ -2,6 +2,8 @@ package com.fen4kin.webapp.storage;
 
 import com.fen4kin.webapp.model.Resume;
 
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -10,49 +12,81 @@ public class ArrayStorage {
     private int size = 0;
 
     public void clear() {
-        for (int i = 0; i < size; i++) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage, null);
         size = 0;
     }
 
+    public void update(Resume r) {
+        if (isPresent(getAll(), r)) {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].equals(r)) {
+                    storage[i].setUuid(r.getUuid());    //единственное поле, которое мы можем обновить, это uuid
+                }
+            }
+        } else System.out.println("Resume has not found");
+    }
+
     public void save(Resume r) {
-        storage[size] = r;
-        size++;
+        if (!isPresent(getAll(), r)) {
+            storage[size] = r;
+            size++;
+        } else System.out.println("Resume already exists");
+
     }
 
     public Resume get(String uuid) {
+        Resume r = null;
+        //поиск резюме по uuid. Если его нет, то временная переменная остается пустой
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
-                return storage[i];
+                r = storage[i];
             }
         }
-        return null;
+        //присутсвует ли резюме в заполненной области
+        if (isPresent(getAll(), r)) return r;
+        else System.out.println("Resume has not found");
+
+        return null;    //без Exception не знаю как обернуть возврат без null
     }
 
     public void delete(String uuid) {
+        Resume r = null;
+
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
-                storage[i] = storage[size - 1];
-                storage[size - 1] = null;
-                size--;
-                break;
+                r = storage[i];
             }
         }
+
+        if (isPresent(getAll(), r)) {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].equals(r)) {
+                    storage[i] = storage[size - 1];
+                    storage[size - 1] = null;
+                    size--;
+                    break;
+                }
+            }
+        } else System.out.println("Resume has not found");
     }
+
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        Resume[] array = new Resume[size];
-        for (int i = 0; i < size; i++) {
-            array[i] = storage[i];
-        }
-        return array;
+        return Arrays.copyOf(storage, size);
     }
 
     public int size() {
         return size;
+    }
+
+    private boolean isPresent(Resume[] temp, Resume r) {
+        for (Resume t : temp) {
+            if (t.equals(r))     //equals и hashCode переопределила автоматически
+                return true;
+        }
+        return false;
     }
 }
