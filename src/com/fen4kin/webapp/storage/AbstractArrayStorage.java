c297a2b4d13a9fff256975a60fba616d066e2d39
fen4kin <fen4kin@gmail.com>
2019-04-12 18:28:51 +0500

    add methods in abstract class implement 2 methods in arraystorage

--- src/com/fen4kin/webapp/storage/AbstractArrayStorage.java
+++ src/com/fen4kin/webapp/storage/AbstractArrayStorage.java
@@ -2,6 +2,8 @@ package com.fen4kin.webapp.storage;
 
 import com.fen4kin.webapp.model.Resume;
 
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -11,8 +13,41 @@ public abstract class AbstractArrayStorage implements Storage {
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
 
-    public int size() {
-        return size;
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index == -1) {                                                  //если резюме не существует
+            System.out.println("Resume " + r.getUuid() + " has not found");
+        } else {
+            storage[index] = r;
+        }
+    }
+
+    public void save(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index != -1) {
+            System.out.println("Resume " + r.getUuid() + " already exists");
+        } else if (size >= STORAGE_LIMIT) {                                //проверка на переполнение как в примере
+            System.out.println("Storage overflow");
+        } else {
+            insertElement(r, index);
+            size++;
+        }
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " has not found");
+        } else {
+            fillDeletedElement(index);
+            storage[size - 1] = null;
+            size--;
+        }
     }
 
     public Resume get(String uuid) {
@@ -24,8 +59,21 @@ public abstract class AbstractArrayStorage implements Storage {
         return storage[index];
     }
 
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0, size);
+    }
+
+    public int size() {
+        return size;
+    }
+
     protected abstract int getIndex(String uuid);
 
+    protected abstract void fillDeletedElement(int index);
 
+    protected abstract void insertElement(Resume r, int index);
 
 }
--- src/com/fen4kin/webapp/storage/ArrayStorage.java
+++ src/com/fen4kin/webapp/storage/ArrayStorage.java
@@ -9,50 +9,7 @@ import java.util.Arrays;
  */
 public class ArrayStorage extends AbstractArrayStorage {
 
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
-
-    public void update(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (index == -1) {                                                  //если резюме не существует
-            System.out.println("Resume " + r.getUuid() + " has not found");
-        } else {
-            storage[index] = r;
-        }
-
-    }
-
-    public void save(Resume r) {
-        if (getIndex(r.getUuid()) != -1) {
-            System.out.println("Resume " + r.getUuid() + " already exists");
-        } else if (size >= STORAGE_LIMIT) {                                //проверка на переполнение как в примере
-            System.out.println("Storage overflow");
-        } else {
-            storage[size] = r;
-            size++;
-        }
-    }
-
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index == -1) {
-            System.out.println("Resume " + uuid + " has not found");
-        } else {
-            storage[index] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
-        }
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size);
-    }
-
+    @Override
     protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].getUuid())) {
@@ -61,4 +18,15 @@ public class ArrayStorage extends AbstractArrayStorage {
         }
         return -1;
     }
+
+    @Override
+    protected void fillDeletedElement(int index) {
+        storage[index] = storage[size - 1];
+    }
+
+    @Override
+    protected void insertElement(Resume r, int index) {
+        storage[size] = r;
+    }
+
 }
