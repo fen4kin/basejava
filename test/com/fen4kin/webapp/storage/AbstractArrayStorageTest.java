28d7f5ef78037b31a46f7c9b3cea116dbe58be82
fen4kin <fen4kin@gmail.com>
2019-06-22 01:53:20 +0500

    add 2 tests without abstract complete

--- test/com/fen4kin/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/fen4kin/webapp/storage/AbstractArrayStorageTest.java
@@ -1,17 +1,27 @@
 package com.fen4kin.webapp.storage;
 
+
+import com.fen4kin.webapp.exception.ExistStorageException;
 import com.fen4kin.webapp.exception.NotExistStorageException;
+import com.fen4kin.webapp.exception.StorageException;
 import com.fen4kin.webapp.model.Resume;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class AbstractArrayStorageTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public abstract class AbstractArrayStorageTest {
     private Storage storage;
 
     private static final String UUID_1 = "uuid1";
     private static final String UUID_2 = "uuid2";
     private static final String UUID_3 = "uuid3";
+
+    protected AbstractArrayStorageTest(Storage storage) {
+        this.storage = storage;
+    }
 
     @Before
     public void setUp() throws Exception {
@@ -22,32 +32,38 @@ public class AbstractArrayStorageTest {
     }
 
     @Test
-    public void size() {
+    public void size() throws Exception {
         Assert.assertEquals(3, storage.size());
     }
 
     @Test
-    public void clear() {
+    public void clear() throws Exception {
+
     }
 
     @Test
-    public void update() {
+    public void update() throws Exception {
+
     }
 
     @Test
-    public void getAll() {
+    public void getAll() throws Exception {
+
     }
 
     @Test
-    public void save() {
+    public void save() throws Exception {
+
     }
 
     @Test
-    public void delete() {
+    public void delete() throws Exception {
+
     }
 
     @Test
-    public void get() {
+    public void get() throws Exception {
+
     }
 
     @Test(expected = NotExistStorageException.class)
