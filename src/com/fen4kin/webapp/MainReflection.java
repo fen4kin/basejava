87bed1ddbd72541b850d5d63fe259d3c5e54c54c
fen4kin <fen4kin@gmail.com>
2019-06-22 20:36:35 +0500

    invoke r.toString via reflection

--- src/com/fen4kin/webapp/MainReflection.java
+++ src/com/fen4kin/webapp/MainReflection.java
@@ -3,10 +3,11 @@ package com.fen4kin.webapp;
 import com.fen4kin.webapp.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 
 public class MainReflection {
 
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
@@ -14,6 +15,8 @@ public class MainReflection {
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
         // TODO : invoke r.toString via reflection
+        //http://qaru.site/questions/14577/how-do-i-invoke-a-java-method-when-given-the-method-name-as-a-string
+        System.out.println((r.getClass().getMethod("toString")).invoke(r));
         System.out.println(r);
     }
 }
